d07d5441866a6d74e54fdad94898c841f3fb5624
Jan Griesfeller <jgriesfeller@users.noreply.github.com>
2018-01-12 08:55:40 +0000

    added proper command line interface

--- GetNewFolder/GetNewFolder.py
+++ GetNewFolder/GetNewFolder.py
@@ -30,13 +30,19 @@ import configparser
 import os
 import time
 import re
+import argparse
+import sys
 
 
-def GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False, DebugFlag=False, c_ConfigFile=None ):
+
+def GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False, DebugFlag=False, c_ConfigFile=None):
 	if c_ConfigFile == None:
-		c_ConfigFile='folders.ini'
+		c_ConfigFile=os.path.join(os.path.dirname(os.path.realpath(__file__)),'../constants.ini')
+		#pdb.set_trace()
 	dict_Config={}
 	now=time.time()
+	if VerboseFlag:
+		sys.stderr.write('reading config file :'+c_ConfigFile+'\n')
 	if os.path.isfile(c_ConfigFile):
 		ReadConfig = configparser.ConfigParser()
 		ReadConfig.read(c_ConfigFile)
@@ -49,7 +55,7 @@ def GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False, DebugFlag=False, c_
 		MatchPattern=re.compile('.*/renamed$')
 		for FolderToSearchIn in dict_Config['FoldersToSearchIn']:
 			if VerboseFlag:
-				print(FolderToSearchIn)
+				sys.stderr.write('checking '+FolderToSearchIn+' ...\n')
 			#get the directories
 			if os.path.isdir(FolderToSearchIn):
 				SubDirs=[x[0] for x in os.walk(FolderToSearchIn)]
@@ -62,11 +68,11 @@ def GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False, DebugFlag=False, c_
 							if DiffDays <= MaxTimeDiffDays:
 								DirsToWorkOn.append(dir)
 								if VerboseFlag:
-									print(dir)
-									print('difference to now: ',(now - os.path.getmtime(dir))/60./60./24.,' days')
+									sys.stderr.write(dir+'\n')
+									#pdb.set_trace()
+									#sys.stderr.write('difference to now: '+str(now - os.path.getmtime(dir))/60./60./24.,+' days'+'\n')
+									sys.stderr.write('difference to now: {:5.2f} days'.format(DiffDays)+'\n')
 
-		if VerboseFlag:
-			print(DirsToWorkOn)
 		if DebugFlag:
 			pdb.set_trace()
 
@@ -74,7 +80,39 @@ def GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False, DebugFlag=False, c_
 
 
 if __name__ == '__main__':
-	MaxTimeDiffDays=2.
-	DirsToWorkOn=GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=False)
-	print(DirsToWorkOn)
+
+	parser = argparse.ArgumentParser(description='aerocom-automation-tools: GetNewFolder.py\nProgram to find the newest models.\n\n')
+	parser.add_argument("--age", help="max file age in days. Defaults to 2.",default=2)
+	parser.add_argument("--verbose", help="switch on verbosity",action='store_true')
+	parser.add_argument("--listmodels", help="list just the model name, not the model folder.",action='store_true')
+	#parser.add_argument("--modelyear", help="model years to run; use 9999 for climatology, leave out for all years; comma separated list")
+
+	args = parser.parse_args()
+
+	if args.listmodels:
+		ListModels=True
+	else:
+		ListModels=False
+	
+	if args.verbose:
+		VerboseFlag=True
+	else:
+		VerboseFlag=False
+	
+	if args.age:
+		MaxTimeDiffDays=int(args.age)
+		if VerboseFlag is True:
+			sys.stderr.write('searching for dirs up to '+str(MaxTimeDiffDays)+' days old.\n')
+	
+	#MaxTimeDiffDays=2.
+	#pdb.set_trace()
+	DirsToWorkOn=GetModelDirsToWorkOn(MaxTimeDiffDays, VerboseFlag=VerboseFlag)
+	if ListModels is False:
+		for Dir in DirsToWorkOn:
+			sys.stdout.write(Dir+'\n')
+	else:
+		for Dir in DirsToWorkOn:
+			DummyArr=Dir.split('/')
+			sys.stdout.write(DummyArr[-2]+'\n')
+			
 
